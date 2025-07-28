9c39e5be52a020c286aa5f087a3d726eec671105
Angel <151583275+Anng3l@users.noreply.github.com>
2025-07-27 19:06:01 -0500

    Avances en dashboard y UI de autenticación hecha

--- auth/loginScripts.js
+++ auth/loginScripts.js
@@ -1,3 +1,7 @@
 document.getElementById("anchorRegister").addEventListener("click", () => {
     window.location.href="./register.html"
 })
+
+document.getElementById("loginButton").addEventListener("click", () => {
+    window.location.href="../dashboard/dashbboard.html"
+})
